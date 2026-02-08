d7a633c266b1ee6f14452629fd1e20722d873516
lsc <dltmd1502@naver.com>
2026-02-09 01:22:59 +0900

    feat: 메인01

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,20 +6,18 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
+	...compat.extends("next/core-web-vitals", "next/typescript"),
+	{
+		ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"],
+		rules: {
+			"@typescript-eslint/no-unused-vars": "warn", //사용하지 않는 변수가 있을 때 경고로 표시해요
+			"@typescript-eslint/no-explicit-any": "off", //any 타입을 명시적으로 정의할 수 있도록 허용해요
+		},
+	},
 ];
 
 export default eslintConfig;
