5a64fe2f523758867b16eb77219a572c0336e342
lsc <dltmd1502@naver.com>
2026-02-10 16:42:23 +0900

    feat: footer추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import ShareButton from "./components/ShareButton";
 import Link from "next/link";
 
 export const metadata: Metadata = {
-	title: "디주릿",
+	title: "DIDYOUREAD | 디쥬릿",
 	description: "읽은 줄 알았지? 공지 읽기 3분 서바이벌",
 };
 
@@ -28,15 +28,25 @@ export default function RootLayout({
 					<header className="header">
 						<div className="header__inner">
 							<h1 className="header__title">
-								<Link href="/">
-									DIDYOUREAD
-								</Link>
+								<Link href="/">DIDYOUREAD</Link>
 							</h1>
 
 							<ShareButton />
 						</div>
 					</header>
 					{children}
+					<footer className="footer">
+						<div className="footer__inner">
+							<ul className="footer-list">
+								<li>
+									<Link href="/">개인정보처리 방침</Link>
+								</li>
+								<li>
+									<p>© 2026 DIDYOUREAD. All rights reserved.</p>
+								</li>
+							</ul>
+						</div>
+					</footer>
 				</section>
 			</body>
 		</html>
